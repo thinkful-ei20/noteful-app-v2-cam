925f6f684a1c8c5d927cd5a32b55e5672b984558
Cam Prier <CSPrier@gmail.com>
2018-04-24 16:22:56 -0700

    Set up all queries in notes-queries.js

--- scratch/notes-queries.js
+++ scratch/notes-queries.js
@@ -2,6 +2,7 @@
 
 const knex = require('../knex');
 
+/*
 let searchTerm = 'Hello';
 knex
   .select('notes.id', 'title', 'content')
@@ -18,3 +19,61 @@ knex
   .catch(err => {
     console.error(err);
   });
+*/
+
+/*
+// Get Note By Id accepts an ID. It returns the note as an object note an array  
+let searchTerm = 3;
+knex
+  .select()
+  .from('notes')
+  .where({ 'id': searchTerm })
+  .then(results => {
+    console.log(JSON.stringify(results[0], null, 2));
+  });
+*/
+
+/*
+// Update Note By Id accepts and ID and an object with the desired updates. It returns the updated note as an object
+let searchTerm = 3;
+
+knex('notes')
+  .select()
+  .where({ 'id': searchTerm })
+  .update({ 'title': 'WHAT THE FU-' })
+  .returning(['id', 'title'])
+  .then(results => {
+    console.log(JSON.stringify(results[0], null, 2));
+  });
+*/
+
+/*
+// Create a Note accepts an object with the note properties and inserts it in the DB. It returns the new note(including the new id) as an object
+knex('notes')
+  .insert([{
+    title: 'Create a Note AHHH',
+    content: 'AHHHhhhhHHH Real Monsters'
+  }])
+  .returning(['id', 'title', 'content'])
+  .then(results => {
+    console.log(JSON.stringify(results[0], null, 2));
+  });
+*/
+
+/*
+let searchTerm = 3;
+// Delete Note By Id accepts an ID and deletes the note from the DB
+knex('notes')
+  .select()
+  .where({ 'id': searchTerm })
+  .del()
+  .then(results => {
+    console.log(JSON.stringify(results, null, 2));
+  });
+*/
+
+knex('notes')
+.select()
+.then(results => {
+  console.log(JSON.stringify(results, null, 2));
+});
