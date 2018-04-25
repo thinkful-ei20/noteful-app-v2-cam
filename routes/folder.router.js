871212c16d02e67f93af2ef2fef160d4dab60d08
Cam Prier <CSPrier@gmail.com>
2018-04-25 16:05:00 -0700

    PUT and POST methods implemented in folder.router.js

--- routes/folder.router.js
+++ routes/folder.router.js
@@ -32,8 +32,70 @@ router.get('/folders/:id', (req, res, next) => {
     .catch(err => next(err));
 });
 
+// Update folder the noteful
+// The noteful app does not use this endpoint but we'll create it in order to round out our API
+router.put('/folders/:id', (req, res, next) => {
+  const id = req.params.id;
 
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateableFields = ['name'];
 
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  /***** Never trust users - validate input *****/
+  if (!updateObj.name) {
+    const err = new Error('Missing `field` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  knex('folders')
+    .select()
+    .where({ 'id': id })
+    .update(updateObj)
+    .returning(['id', 'name'])
+    .then(item => {
+      if (item) {
+        res.json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+// Create a Folder accepts an object with a name and inserts it in the DB. Returns the new item along with the new ID.
+router.post('/folders', (req, res, next) => {
+  const { name } = req.body;
+
+  const newFolder = { name };
+
+  /***** Never trust users - validate input *****/
+  if (!newFolder.name) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  knex('folders')
+    .insert([{ name }])
+    .returning(['id', 'name'])
+    .then(item => {
+      if (item) {
+        res.location(`http://${req.headers.host}/folders/${item.id}`).status(201).json(item);
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
 
 module.exports = router;
